caab412d1a7e8cc042b1055b402f433605cf8234
Guilherme Bigois <guilhermebigois@outlook.com>
2026-09-14 18:16:17 -0300

    feat: add books endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 // Import the Express module
 import express from "express";
 
+// Import data from the JSON file
+import books from "../data/books.json" with { type: "json" };
+
 // Create an instance of an Express application
 const app = express();
 
@@ -11,6 +14,11 @@ const HTTP_NOT_FOUND = 404;
 // Define a route for the root URL ("/")
 app.get("/", (req, res) => {
     res.status(HTTP_OK).send("Hello, World!");
+});
+
+// Define a route for retrieving all books
+app.get("/books", (req, res) => {
+    res.status(HTTP_OK).type("application/json").json(books);
 });
 
 // Handle 404 errors for undefined routes
