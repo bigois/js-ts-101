66ffde6cf5f552319d9a201ca8cc58296e12d011
Guilherme Bigois <guilhermebigois@outlook.com>
2026-09-14 17:42:11 -0300

    feat: add route response mapping

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ import http from "http";
 // Define the port number for the server to listen on
 const PORT = 3000;
 
+// Define the routes for the server
+const ROUTES = {
+    "/": "Hello, World!"
+};
+
 // Create an HTTP server instance
 const server = http.createServer((req, res) => {
     // res.statusCode = 200;
     // res.setHeader("Content-Type", "text/plain");
     res.writeHead(200, { "Content-Type": "text/plain" });
 
-    res.end("Hello, World!\n");
+    // Send the response based on the requested URL
+    res.end(ROUTES[req.url] || "Not Found\n");
 });
 
 // Start the server and listen on port 3000
