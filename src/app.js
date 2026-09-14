92165a2f84a9b172abeecb94e2b6432b1d4bb168
Guilherme Bigois <guilhermebigois@outlook.com>
2026-09-14 18:15:11 -0300

    refactor: define HTTP status code constants

--- src/app.js
+++ src/app.js
@@ -4,14 +4,18 @@ import express from "express";
 // Create an instance of an Express application
 const app = express();
 
+// Define HTTP status codes
+const HTTP_OK = 200;
+const HTTP_NOT_FOUND = 404;
+
 // Define a route for the root URL ("/")
 app.get("/", (req, res) => {
-    res.status(200).send("Hello, World!");
+    res.status(HTTP_OK).send("Hello, World!");
 });
 
 // Handle 404 errors for undefined routes
 app.use((req, res) => {
-    res.status(404).send("Not Found");
+    res.status(HTTP_NOT_FOUND).send("Not Found");
 });
 
 // Export the Express application instance for use in other modules
